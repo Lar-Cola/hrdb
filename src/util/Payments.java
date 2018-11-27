d460918ec782a716459bef2c9b40803deb17a360
Marc Benigno <mjgbenigno@gmail.com>
2018-11-27 00:23:55 -0600

    expanded payments for use with both objects and standalone

--- src/util/Payments.java
+++ src/util/Payments.java
@@ -12,12 +12,35 @@ public class Payments {
 	public Payments() {
 	}
 	
+	/**
+	 * Simple payroll calculation without overtime hours
+	 * @param e
+	 * @param hours
+	 * @return
+	 */
 	public double getPayPerHour(Employee e, double hours) {
 		return e.getEmployeePayRate() * hours;
 	}
 	
+	
+	/**
+	 * Simple payroll calculation with overtime hours
+	 * @param e
+	 * @param hours
+	 * @param overtimeHours
+	 * @return
+	 */
 	public double getPayPerHour(Employee e, double hours, double overtimeHours) {
 		return (e.getEmployeePayRate()* hours) + (e.getOvertimeRate() * overtimeHours);
+	}
+	
+	
+	public double getPayPerHour(double payrate, double hours) {
+		return payrate * hours; 
+	}
+	
+	public double getPayPerHour(double payrate, double hours , double overtimeRate, double overtimeHours) {
+		return (payrate * hours + (overtimeRate * overtimeHours));
 	}
 	
 }
