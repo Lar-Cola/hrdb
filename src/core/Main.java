c812933014595447155e3f063c3cc69ea1f19293
Marc Benigno <mjgbenigno@gmail.com>
2018-11-27 18:06:26 -0600

    minor edit to show GUI

--- src/core/Main.java
+++ src/core/Main.java
@@ -8,6 +8,7 @@ public class Main {
 	public static void main(String[] args) {
 		System.out.println("This system works if you see initialization messages below.");
 		g = new GUI(debugFlag);
+		g.setVisible(true);
 		b = new Bridge(debugFlag);
 	}
 }
