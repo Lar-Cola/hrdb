92fcc4f0849faf4413eeafcfd7b4b0a467684978
Marc Benigno <mjgbenigno@gmail.com>
2018-11-29 15:24:13 -0600

    modified benefit class
    added setter/getter methods for benefitType.
    
    changed return value for getBenefitID

--- src/units/Benefit.java
+++ src/units/Benefit.java
@@ -5,6 +5,7 @@ public class Benefit {
 	//necessary variables
 	private String benefitName = null;
 	private int benefitID = -1;
+	private String benefitType = null;
 	
 	public Benefit(String name, int ID) {
 		this.benefitName = name;
@@ -19,11 +20,19 @@ public class Benefit {
 		this.benefitID = ID;
 	}
 	
+	public void setBenefitType(String type) {
+		this.benefitType = type;
+	}
+	
 	public String getBenefitName() {
 		return this.benefitName;
 	}
 	
-	public long getBenefitID() {
+	public int getBenefitID() {
 		return this.benefitID;
+	}
+	
+	public String getBenefitType() {
+		return this.benefitType;
 	}
 }
